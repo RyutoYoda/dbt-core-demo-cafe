c95a6d5522b72893ae8b75cf3c12e2cc632083c7
RyutoYoda❄️ <98861847+RyutoYoda@users.noreply.github.com>
2025-07-07 23:15:58 +0900

    Update stg_stores.sql

--- models/staging/stg_stores.sql
+++ models/staging/stg_stores.sql
@@ -5,22 +5,9 @@
 - カラム名の変更: 日本語のカラム名を英語にリネームしています。
 */
 
-with source as (
-
-    select * from {{ source('main', '店舗マスタ') }}
-
-),
-
-renamed as (
-
-    select
-        "店舗ID" as store_id,
-        "店舗名" as store_name,
-        "所在地" as location,
-        "オープン日" as opened_at
-
-    from source
-
-)
-
-select * from renamed
+select
+    "店舗ID" as store_id,
+    "店舗名" as store_name,
+    "所在地" as location,
+    "オープン日" as opened_at
+from {{ source('main', '店舗マスタ') }}
