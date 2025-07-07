aea23b12658e330b63a14fd09e889316a3c53c64
RyutoYoda❄️ <98861847+RyutoYoda@users.noreply.github.com>
2025-07-07 23:13:01 +0900

    Update stg_products.sql

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -5,23 +5,12 @@
 - カラム名の変更: 日本語のカラム名を英語にリネームしています。
 */
 
-with source as (
+select
+    "商品ID" as product_id,
+    "商品名" as product_name,
+    "カテゴリ" as category,
+    "価格" as price,
+    "カスタマイズ有無" as is_customizable
 
-    select * from {{ source('main', '商品マスタ') }}
+from {{ source('main', '商品マスタ') }}
 
-),
-
-renamed as (
-
-    select
-        "商品ID" as product_id,
-        "商品名" as product_name,
-        "カテゴリ" as category,
-        "価格" as price,
-        "カスタマイズ有無" as is_customizable
-
-    from source
-
-)
-
-select * from renamed
